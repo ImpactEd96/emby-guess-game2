95de8def1b6cac20cf3c721eb4385e87ce75a174
Eddie <eddie.kimpton@live.com.au>
2026-08-18 14:41:52 +0930

    added masterPlaylistUrl

--- src/gameroom.ts
+++ src/gameroom.ts
@@ -378,6 +378,7 @@ export class GameRoom {
 
     // Get HLS master playlist
     const playSessionId = crypto.randomUUID();
+    const masterPlaylistUrl = `${this.env.EMBY_URL}/Videos/${movieId}/master.m3u8?MediaSourceId=${mediaSourceId}&Static=false&VideoCodec=h264&AudioCodec=aac&VideoBitRate=2000000&AudioBitRate=128000&MaxStreamingBitrate=2000000&PlaySessionId=${playSessionId}`;
     const masterResponse = await fetch(masterPlaylistUrl, {
       headers: { 'X-Emby-Token': this.env.EMBY_API_KEY },
     });
